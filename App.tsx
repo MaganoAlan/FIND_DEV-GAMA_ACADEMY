9dd365afe8ad121b5292eeff45086ea0baf9bfa1
Lucas Guimarães <lucasmg09@hotmail.com>
2022-09-05 21:17:15 -0300

    AWS Integration

--- App.tsx
+++ App.tsx
@@ -1,4 +1,8 @@
 import Router from './src/router'
+import { Amplify } from 'aws-amplify'
+
+import awsconfig from './src/aws-exports'
+Amplify.configure(awsconfig)
 
 export default function App() {
   return <Router />;
---
+++ src/models/schema.js
@@ -0,0 +1,66 @@
+export const schema = {
+    "models": {
+        "User": {
+            "name": "User",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "name": {
+                    "name": "name",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Users",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "public",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "db1ed9ed820c6c905946ace8a0235b6a"
+};
