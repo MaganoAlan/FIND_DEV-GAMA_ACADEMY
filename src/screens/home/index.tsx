226690164743bf0d7f005abc0a591b7988e125fd
Alan Magano <alan.magano@kukac.com.br>
2022-09-14 19:47:42 -0300

    update: layout login routes

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,41 +1,9 @@
-import Button from "../../components/button";
-import {
-  Container,
-  DevContainer,
-  DevImage,
-  ProgrammerImage,
-  FindDevContainer,
-  Title,
-  Subtitle,
-  KnowMoreContainer,
-  KnowMoreText,
-  Buttons,
-} from "./styles";
+import { Container, Title } from "./styles";
 
 export default function Home({ navigation }) {
-  function handlePressKnowMore() {
-    //TODO: Trocar para rota correta
-
-    /* navigation.navigate("signIn"); */
-    console.log("Know more");
-  }
-
   return (
     <Container>
-      <DevContainer>
-        <DevImage source={require("../../assets/images/dev_1.png")} />
-        <DevImage source={require("../../assets/images/dev_2.png")} />
-        <DevImage source={require("../../assets/images/dev_3.png")} />
-      </DevContainer>
-      <ProgrammerImage source={require("../../assets/images/programmer.png")} />
-      <FindDevContainer>
-        <Title>Find Dev</Title>
-        <Subtitle>The best place to find a tech talent</Subtitle>
-        <Buttons>
-          <Button title="Login" background="primary" onPress={() => {}} />
-          <Button title="Cadastrar" background="secondary" onPress={() => {}} />
-        </Buttons>
-      </FindDevContainer>
+      <Title>Home</Title>
     </Container>
   );
 }
--- src/screens/signIn/index.tsx
+++ src/screens/signIn/index.tsx
@@ -1,11 +1,39 @@
-import { Text } from "react-native";
+import Button from "../../components/button";
+import {
+  Container,
+  DevContainer,
+  DevImage,
+  ProgrammerImage,
+  FindDevContainer,
+  Title,
+  Subtitle,
+  Buttons,
+} from "./styles";
 
-import { Container } from "./styles";
+export default function Home({ navigation }) {
+  function handlePressKnowMore() {
+    //TODO: Trocar para rota correta
 
-export default function SignIn() {
+    /* navigation.navigate("signIn"); */
+    console.log("Know more");
+  }
+
   return (
     <Container>
-      <Text>SignIn Screen</Text>
+      <DevContainer>
+        <DevImage source={require("../../assets/images/dev_1.png")} />
+        <DevImage source={require("../../assets/images/dev_2.png")} />
+        <DevImage source={require("../../assets/images/dev_3.png")} />
+      </DevContainer>
+      <ProgrammerImage source={require("../../assets/images/programmer.png")} />
+      <FindDevContainer>
+        <Title>Find Dev</Title>
+        <Subtitle>The best place to find a tech talent</Subtitle>
+        <Buttons>
+          <Button title="Login" background="primary" onPress={() => {}} />
+          <Button title="Cadastrar" background="secondary" onPress={() => {}} />
+        </Buttons>
+      </FindDevContainer>
     </Container>
   );
 }
