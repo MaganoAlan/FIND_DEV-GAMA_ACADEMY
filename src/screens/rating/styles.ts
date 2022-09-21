661877680978aff3e8c7a07174d166134539ca5b
binaryleo <leomouracss@fastmail.com>
2022-09-21 16:23:35 -0300

    feat: add rating screen - initial layout

---
+++ src/screens/rating/styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components/native";
+
+export const StyledImage = styled.Image`
+  width: 100%;
+  height: 25%;
+`;
+
+export const Title = styled.View`
+  margin-top: 5%;
+  width: 35%;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+export const Promo = styled.View`
+  margin-top: 2%;
+  width: 80%;
+  align-items: center;
+ 
+
+`
+export const ButtonsContainer = styled.View`
+  width: 100%;
+  flex-direction: column;
+`;
+
+export const FooterLogo = styled.Image`
+  bottom: 0;
+  margin-top: 5%;
+`;
+export const Perks = styled.View`
+  width: 80%;
+  margin-top: 15%;
+  align-items: center;
+  justify-content: center;
+`;
+export const Label = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  margin-bottom: 5%;
+  text-align: center;
+`;
+
+export const ButtonContainer = styled.View`
+  width: 45%;
+  margin-top: 3%;
+`;
